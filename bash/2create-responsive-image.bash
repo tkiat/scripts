f130ffb837f71ec2b3bd747d99c3b930b71aff23
Theerawat Kiatdarakun <tkiatd@outlook.com>
2020-09-09 22:14:52 +0700

    modify 2create-re...

--- bash/2create-responsive-image.bash
+++ bash/2create-responsive-image.bash
@@ -4,33 +4,36 @@ filename="${filename_and_ext%%.*}"
 ext="${filename_and_ext#*.}"
 
 read w h <<< $(identify -format '%w %h' $1)
-if [ $(($w % $2)) -eq "0" ] && [ $(($h % $2)) -eq "0" ]; then
+if [ $2 -ne "0" ] && [ $(($w % $2)) -eq "0" ] && [ $(($h % $2)) -eq "0" ]; then
 	let stepW=$(($w / $2))
 	let stepH=$(($h / $2))
 	let cur=1
 	for i in $(seq 1 $2); do
 		convert $1 -resize $(($stepW * $i))x$(($stepH * $i)) ${filename}-$(($stepW * $i))x$(($stepH * $i)).${ext}
 	done
-	# rm $1
 	# display img tag
-	source="https://www.dummy.com"
-	noob=""
+# 	IFS=/
+# 	read -ra val <<< $(pwd)
+# 	article_dir="${val[*]:(-4)}"
+# 	IFS=$'\n'
+	source="{img-url}" # AsciiDoctor's variables
+	srcset=""
 	for i in $(seq 1 $2); do
-		noob="$noob
+		srcset="$srcset
 $source/$filename-$(($stepW * $i))x$(($stepH * $i)).$ext $(($stepW * $i))w"
 		if [ $i -ne $2 ]; then
-			noob="$noob,"
+			srcset="$srcset,"
 		else
-			noob="$noob\"";
+			srcset="$srcset\"";
 		fi
 	done
 	cat<<-EOF
-	<img srcset="$noob
+	<img srcset="$srcset
 		sizes="(max-width: $(($w + 100))px) 70vw,
 		${w}px"
 		src="${source}/${filename}-${w}x${h}.${ext}"
 		alt="&lt;img2&gt;">
 	EOF
 else
-	echo "cannot resize image: width or height are not divisible by $2"
+	echo "cannot resize image: width ($w px) or height ($h px) are not divisible by $2"
 fi
