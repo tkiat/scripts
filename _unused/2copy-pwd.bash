c0464e1fca516968cca279670d4cc7c4566569f2
Theerawat Kiatdarakun <tkiat@tutanota.com>
2021-02-12 15:38:45 +0700

    organize

--- _unused/2copy-pwd.bash
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-#pwd | echo -n "$(xargs -0 echo)" | xclip -selection clipboard
-pwd_escaped=$(echo $(pwd) | sed 's/"/\\"/g; s/\x27/\\\x27/g; s/ /\\ /g')
-echo -n "$pwd_escaped" | xclip -selection clipboard
--- _unused/prepend_zero_to_int.py
+++
@@ -1,2 +0,0 @@
-def prepend_zero_to_int(num, digit):
-    return str(10 ** digit + num)[-digit:]
